e0726d8498945c957b85b7afcb95127420937b54
baoyukun <bykmail1992@163.com>
2022-09-17 02:06:11 +0800

    长按保存

---
+++ src/views/home/components/tools/export/drawer/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>长按保存图片</div>
+  <div class="img-wrap">
+    <img :src='src' />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { inject } from 'vue'
+
+const cvs = inject('cvs')
+const src = cvs.dom.toDataURL()
+</script>
+
+<style scoped lang="scss">
+.img-wrap {
+  padding: 20px;
+  text-align: center;
+  img {
+    max-width: 80%;
+  }
+}
+</style>
--- src/views/home/components/tools/export/index.vue
+++ src/views/home/components/tools/export/index.vue
@@ -1,20 +1,22 @@
 <template>
-  <span @click="download">
+  <span @click="openDrawer(Drawer)">
     <span class="iconfont icon-download"></span> 导出图片
   </span>
 </template>
 
 <script setup lang="ts">
 import { inject } from 'vue'
+import Drawer from './drawer/index.vue'
+const openDrawer = inject('openDrawer')
 
-const cvs = inject('cvs')
-const download = () => {
-  const a = document.createElement('a')
-  a.download = Date.now() + '.png'
-  a.href = cvs.dom.toDataURL()
-  const e = new MouseEvent('click')
-  a.dispatchEvent(e)
-}
+// const cvs = inject('cvs')
+// const download = () => {
+//   const a = document.createElement('a')
+//   a.download = Date.now() + '.png'
+//   a.href = cvs.dom.toDataURL()
+//   const e = new MouseEvent('click')
+//   a.dispatchEvent(e)
+// }
 </script>
 
 <style scoped lang='scss'>
